c0c8185befb77899e87e79b90fc11d1177ec47bd
neker94 <borja_villarino@hotmail.com>
2017-02-04 18:22:37 +0100

    Object class v1

--- P5GLSL/Object.cpp
+++ P5GLSL/Object.cpp
@@ -1,4 +1,5 @@
 #include "Object.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 
 Object::Object()
@@ -8,4 +9,98 @@ Object::Object()
 
 Object::~Object()
 {
+}
+
+void Object::InitObject()
+{
+	m_modelMat = glm::mat4(1.0f);
+	m_normalMat = glm::inverse(glm::transpose(m_modelMat));
+}
+
+glm::mat4 Object::GetModelMatrix()
+{
+	return m_modelMat;
+}
+
+glm::mat4 Object::GetNormalMatrix()
+{
+	return m_normalMat;
+}
+
+
+glm::mat4 Object::GetPosMatrix()
+{
+	return m_posMat;
+}
+
+glm::mat4 Object::GetRotMatrix()
+{
+	return m_rotMat;
+}
+
+glm::mat4 Object::GetScaleMatrix()
+{
+	return m_scaleMat;
+}
+
+//Ejecuta en bucle de lógica
+void Object::Update(float dt) 
+{
+	if (!m_active)
+		return;
+
+	if (!m_useAltMat)
+	{
+		m_modelMat = glm::mat4(1.0f);
+		m_modelMat = glm::translate(m_modelMat, m_globalPosition);
+		m_modelMat = glm::rotate(m_modelMat, m_eulerAngles.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		m_modelMat = glm::rotate(m_modelMat, m_eulerAngles.y, glm::vec3(0.0f, 1.0f, 0.0f));
+		m_modelMat = glm::rotate(m_modelMat, m_eulerAngles.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		m_modelMat = glm::scale(m_modelMat, m_globalScale);
+	}
+	else
+	{
+		m_modelMat = m_altModelMat;
+	}
+
+	m_normalMat = glm::inverse(glm::transpose(m_modelMat));
+}
+
+void Object::SetPosition(glm::vec3 position)
+{
+	m_globalPosition = position;
+}
+
+void Object::SetScale(glm::vec3 scale)
+{
+	m_globalScale = scale;
+}
+
+void Object::SetEulerRotation(glm::vec3 eulerAngles) //Usar rol, pitch y yaw
+{
+	m_eulerAngles = eulerAngles;
+}
+
+
+//Para matrices modelo con transformaciones complejas (sesgado, rotar alrededor de un punto, etc)
+void Object::UseAlternativeModelMat(bool use)
+{
+	m_useAltMat = use;
+}
+
+void Object::SetAlternativeModelMat(glm::mat4 modelMat)
+{
+	m_altModelMat = modelMat;
+}
+
+
+bool Object::SetActive(bool active)
+{
+	m_active = active;
+	return m_active;
+}
+
+bool Object::IsActive()
+{
+	return m_active;
 }
--- P5GLSL/Object.h
+++ P5GLSL/Object.h
@@ -1,8 +1,49 @@
 #pragma once
+#include <glm\glm.hpp>
 class Object
 {
 public:
 	Object();
 	~Object();
+
+	void InitObject();
+
+	glm::mat4 GetModelMatrix();
+	glm::mat4 GetNormalMatrix();
+
+	glm::mat4 GetPosMatrix();
+	glm::mat4 GetRotMatrix();
+	glm::mat4 GetScaleMatrix();
+
+	virtual void Update(float dt); //Metodo preparado para ser overrided, así se pueden crear nuevas clases de objetos con comportamientos especiales
+
+	void SetPosition(glm::vec3 position);
+	void SetScale(glm::vec3 scale);
+	void SetEulerRotation(glm::vec3 eulerAngles); //Usar roll, pitch y yaw
+
+	//Para matrices modelo con transformaciones complejas (sesgado, rotar alrededor de un punto, etc)
+	void UseAlternativeModelMat(bool use);
+	void SetAlternativeModelMat(glm::mat4 modelMat); 
+
+	bool SetActive(bool active);
+	bool IsActive();
+
+private:
+	Object* parent; //¿Crear herencia entre objetos para heredar transformaciones?
+	bool m_active; //Si no está activo el objeto sigue en memoria, pero no se dibuja ni actualiza
+
+	glm::vec3 m_globalPosition;
+	glm::vec3 m_globalScale;
+	glm::vec3 m_eulerAngles;
+	
+	glm::mat4 m_posMat;
+	glm::mat4 m_scaleMat;
+	glm::mat4 m_rotMat;
+
+	glm::mat4 m_modelMat;
+	glm::mat4 m_altModelMat;
+	glm::mat4 m_normalMat;
+
+	bool m_useAltMat = false;
 };
 
